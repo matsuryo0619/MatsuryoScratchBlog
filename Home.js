5d0a889eef17e4d456e4303fe7ce4863a6db26f7
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-30 15:34:51 +0900

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,1 +1,5 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElemtnByID("icon_img").addEventListener("click", function () {
+    window.open("https://scratch.mit.edu", "_blank");
+  });
+});
