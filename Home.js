d0d771bd052412ce975acfa0cfe13427ad7f5e10
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-31 13:42:15 +0900

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,8 +3,3 @@ document.addEventListener("DOMContentLoaded", function () {
     window.open("https://scratch.mit.edu", "_blank");
   });
 });
-
-function newpage(Page) {
-  const Page_URL = "https://matsuryo0619.github.io/MatsuryoScratchBlog/" + Page;
-  window.open(Page_URL);
-}
