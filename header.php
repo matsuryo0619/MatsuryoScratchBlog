acdcee11b0daa082fbb30caaf384a9ad363b5ed1
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-31 09:56:38 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,19 @@
-
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>Scratchブログ</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Home.css">
+    <script src="Home.js"></script>
+  </head>
+  <body>
+    <header>
+     <img src="Logo.png" id="icon_img">
+      <ul id="header_menu">
+        <li class="header_list">ホーム</li>
+        <li class="header_list">このサイトについて</li>
+      </ul>
+    </header>
+    
+  </body>
+</html>
