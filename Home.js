82eef27e03b453ebe5ed13b193496a24ee02806d
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-31 13:09:02 +0900

    Update Home.js

--- Home.js
+++ Home.js
@@ -4,15 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const buttonHome = document.getElementById("button_home");
-
-  if (buttonHome) {
-    buttonHome.addEventListener("click", function () {
-      const url = "https://matsuryo0619.github.io/MatsuryoScratchBlog/%E3%81%93%E3%81%AE%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6.html";
-      window.open(url, "_blank");
-    });
-  } else {
-    console.error("Element with ID 'button_home' not found.");
-  }
-});
