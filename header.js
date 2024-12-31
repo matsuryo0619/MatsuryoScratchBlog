fd8eedc581b012e72cf1586ac18274828cd8a934
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-31 14:31:09 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,19 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // ヘッダーのリスト項目をすべて取得
-  const headerListItems = document.querySelectorAll(".header_list");
-  
-  // 各項目にクリックイベントを追加
-  headerListItems.forEach(item => {
+  // `<body>`内の一番上に新しい内容を追加
+  document.body.insertAdjacentHTML('afterbegin', `
+    <header>
+      <img src="Logo.png" id="icon_img">
+      <ul id="header_menu">
+        <li class="header_list" data-url="Home.html">ホーム</li>
+        <li class="header_list" data-url="about.html">このサイトについて</li>
+      </ul>
+    </header>
+  `);
+
+  // メニュー項目のクリックイベントを追加
+  document.querySelectorAll(".header_list").forEach(item => {
     item.addEventListener("click", function () {
       const url = this.getAttribute("data-url");
       if (url) {
-        window.location.href = "https://matsuryo0619.github.io/MatsuryoScratchBlog/" + url; // 現在のタブでURLを開く
+        window.location.href ="https://matsuryo0619.github.io/MatsuryoScratchBlog/" + url; // 現在のタブでURLを開く
       }
     });
   });
 
   // アイコンのクリックイベント
   document.getElementById("icon_img").addEventListener("click", function () {
-    window.open("https://scratch.mit.edu", "_blank");
+    window.open("https://scratch.mit.edu");
   });
 });
