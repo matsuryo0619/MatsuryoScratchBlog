afa43f204ec114aa04720c87a7130808f2be1830
matsuryo0619 <ryo.matsu0619@gmail.com>
2024-12-31 17:39:42 +0900

    Update about.js

--- about.js
+++ about.js
@@ -1,4 +1,1 @@
-window.addEventListener('load', function() {
-    var headerHeight = document.getElementById('header').offsetHeight;
-    document.getElementById('content').style.marginTop = headerHeight + 'px';
-  });
+
