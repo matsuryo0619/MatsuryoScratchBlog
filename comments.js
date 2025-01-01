105e16ba895e6799f2e7ded5760b05d6c46917ef
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-01 10:10:00 +0900

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,1 +1,18 @@
-
+var NGComments = ["死ね","バカ","クソ"];
+var regex = new RegExp(NGComments.join("|"));
+function test(wcheck) {
+  if (wcheck.match(regex) != null) {
+    alert("NGワードが含まれています｡");
+    return false;
+  }
+  document.getElementById("submitbutton").disabled = true;
+  textareas = document.getElementById('textarea');
+  for(var i=0 ;i < textareas.length ;i ++ ) {
+    textareas[i].value = textareas[i].value.replace( /</g ,'&lt;' );
+	}
+  inputs = document.getElementsByTagName('input');
+	for(var i=0 ;i < inputs.length ;i ++ ){
+		inputs[i].value = inputs[i].value.replace( /</g ,'&lt;' );
+	}
+	return submitted=!0;
+}
