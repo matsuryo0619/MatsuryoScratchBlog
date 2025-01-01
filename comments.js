3ede189102bcba52ab7052776318d6d757df2e1b
matsuryo0619 <ryo.matsu0619@gmail.com>
2025-01-01 10:12:00 +0900

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,16 +1,16 @@
-var NGComments = ["死ね","バカ","クソ"];
+var NGComments = ["死ね","バカ",".exe"]; // 簡易的なNGワードの設定
 var regex = new RegExp(NGComments.join("|"));
 function test(wcheck) {
-  if (wcheck.match(regex) != null) {
-    alert("NGワードが含まれています｡");
-    return false;
-  }
-  document.getElementById("submitbutton").disabled = true;
-  textareas = document.getElementById('textarea');
-  for(var i=0 ;i < textareas.length ;i ++ ) {
-    textareas[i].value = textareas[i].value.replace( /</g ,'&lt;' );
+	if (wcheck.match(regex) != null) {
+		alert("ERROR: コメントにNGワードが含まれています");
+		return false;
 	}
-  inputs = document.getElementsByTagName('input');
+	document.getElementById("submitbutton").disabled = true;
+	textareas = document.getElementsByTagName('textarea');
+	for(var i=0 ;i < textareas.length ;i ++ ){
+		textareas[i].value = textareas[i].value.replace( /</g ,'&lt;' );
+	}
+	inputs = document.getElementsByTagName('input');
 	for(var i=0 ;i < inputs.length ;i ++ ){
 		inputs[i].value = inputs[i].value.replace( /</g ,'&lt;' );
 	}
